4ba929fa76c9e69410ac41b97ea69782d98004e4
Sayan <92419099+reddevil212@users.noreply.github.com>
2024-12-18 19:03:21 +0530

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,20 +1,11 @@
 import os
 import yt_dlp
 from flask import Flask, jsonify, request
-from flask_cors import CORS
-from ytmusicapi import YTMusic
 from werkzeug.utils import secure_filename
-import requests
-import socket
 import tempfile
-from google.cloud import storage
-import base64
+import requests
 
 app = Flask(__name__)
-CORS(app)
-
-# Initialize YTMusic API
-ytmusic = YTMusic()
 
 # Configure a temporary directory for storing uploaded files
 TEMP_DIR = tempfile.mkdtemp()
@@ -109,77 +100,5 @@ def health_check():
     except Exception as e:
         return jsonify({"status": "fail", "message": f"Health check failed: {str(e)}"}), 503
 
-@app.route('/search', methods=['GET'])
-def search():
-    query = request.args.get('query')
-    results = ytmusic.search(query)
-    return jsonify(results)
-
-@app.route('/search_suggestions', methods=['GET'])
-def search_suggestions():
-    query = request.args.get('query')
-    detailed_runs = request.args.get('detailed_runs', default=False, type=lambda x: (x.lower() == 'true'))
-
-    if not query:
-        return jsonify({'error': 'Query parameter is required'}), 400
-
-    try:
-        suggestions = ytmusic.get_search_suggestions(query, detailed_runs)
-        return jsonify(suggestions)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-@app.route('/get_artist', methods=['GET'])
-def get_artist():
-    artist_id = request.args.get('artistId')
-    if not artist_id:
-        return jsonify({'error': 'Artist ID parameter is required'}), 400
-
-    try:
-        artist_info = ytmusic.get_artist(artist_id)
-        return jsonify(artist_info)
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-@app.route('/artists/<string:artist_id>/albums', methods=['GET'])
-def get_artist_albums(artist_id):
-    results = ytmusic.get_artist_albums(artist_id)
-    return jsonify(results)
-
-@app.route('/albums/<string:album_id>', methods=['GET'])
-def get_album(album_id):
-    results = ytmusic.get_album(album_id)
-    return jsonify(results)
-
-@app.route('/albums/<string:album_id>/browse_id', methods=['GET'])
-def get_album_browse_id(album_id):
-    results = ytmusic.get_album_browse_id(album_id)
-    return jsonify(results)
-
-@app.route('/songs/<string:song_id>', methods=['GET'])
-def get_song(song_id):
-    results = ytmusic.get_song(song_id)
-    return jsonify(results)
-
-@app.route('/songs/<string:song_id>/related', methods=['GET'])
-def get_song_related(song_id):
-    results = ytmusic.get_song_related(song_id)
-    return jsonify(results)
-
-def find_available_port(start_port=5000, max_tries=10):
-    """Try to find an available port starting from `start_port`."""
-    for port in range(start_port, start_port + max_tries):
-        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-            if s.connect_ex(('127.0.0.1', port)) != 0:
-                return port
-    return None
-
 if __name__ == '__main__':
-    # Find available port
-    port = find_available_port()
-
-    if port:
-        print(f"Running on port {port}")
-        app.run(debug=True, host='0.0.0.0', port=port)
-    else:
-        print("No available port found.")
+    app.run(debug=True, host='0.0.0.0', port=5000)
